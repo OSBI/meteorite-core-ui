26f450c6ccf3565ace01062e8fae15c83dce72b5
breno.polanski <breno.polanski@gmail.com>
2015-10-06 19:45:21 -0300

    SR-24 #comment Add webpack

---
+++ src/main/resources/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: '',
+	output: {
+		filename: ''
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel'
+			}
+		]
+	}
+};
