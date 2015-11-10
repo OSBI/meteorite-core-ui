09c02151e02c6fa34dad6b41c0da2c5c7053b8ed
breno.polanski <breno.polanski@gmail.com>
2015-11-10 20:39:24 -0300

    SR-24 #comment Update Toolbar component

--- src/main/resources/src/js/saiku/components/Toolbar/ToolbarItem.jsx
+++ src/main/resources/src/js/saiku/components/Toolbar/ToolbarItem.jsx
@@ -5,16 +5,10 @@ var React = require('react');
 var ToolbarItem = React.createClass({
 	render: function() {
 		var item = this.props.item;
-
-		// return (
-		// 	{item.map(function(item) {
-		// 		return <a class="item"><i class={item.icon}></i></a>
-		// 	})}
-		// );
 		
 		return (
 			<a className="item">
-				<i className="fa fa-file"></i>
+				<i className={item.icon}></i>
 			</a>
 		);
 	}
