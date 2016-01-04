ed703d3cc9382f8716693aeda236ac1a026a1775
breno.polanski <breno.polanski@gmail.com>
2016-01-04 10:17:49 -0300

    SR-61 #comment Add in webpack: compiles ES6 (with Babel)

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -1,27 +1,65 @@
-'use strict';
-
-var path    = require('path');
 var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+var WebpackDevServer = require('webpack-dev-server');
+var path = require('path');
 
-module.exports = {
-    cache: true,
-    debug: true,
-    devtool: 'eval',
-    entry: './src/js/saiku/index.jsx',
-    output: {
-        path: path.join(__dirname, 'saiku'),
-        filename: 'saiku.min.js'
-    },
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel'
-			}
-		]
-	},
-    resolve: {
-    	extensions: ['', '.js', '.jsx']
-    }
+var appName = 'saiku';
+var host = '127.0.0.1';
+var port = '9000';
+
+var plugins = [];
+var outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = appName + '.min.js';
+} 
+else {
+  outputFile = appName + '.js';
+}
+
+var config = {
+  entry: './src/js/saiku/index.jsx',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist/saiku',
+    filename: outputFile
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: plugins
 };
+
+if (env === 'dev') {
+  new WebpackDevServer(webpack(config), {
+    contentBase: './',
+    hot: true,
+    debug: true
+  }).listen(port, host, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log('------------------------------------------------------');
+  console.log('Local web server runs at http://' + host + ':' + port);
+  console.log('------------------------------------------------------');
+}
+
+module.exports = config;
