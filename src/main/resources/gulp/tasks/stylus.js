d462d0bd7da74620d007a1c3b5501e4878a30bc2
breno.polanski <breno.polanski@gmail.com>
2016-01-05 17:33:42 -0300

    Add gulp task: stylus

---
+++ src/main/resources/gulp/tasks/stylus.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Necessary plugins
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var stylus  = require('gulp-stylus');
+var paths   = require('../paths');
+
+// Call Stylus
+module.exports = gulp.task('stylus', function() {
+  return gulp.src(paths.source.files.styl)
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest(paths.build.app));
+});
