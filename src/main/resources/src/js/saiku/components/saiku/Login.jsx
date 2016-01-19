7f8d85d798c3c1ea87a23b5c1320143824ac6fa5
breno.polanski <breno.polanski@gmail.com>
2016-01-18 21:05:28 -0300

    SR-73 #comment Create routing system with React

---
+++ src/main/resources/src/js/saiku/components/saiku/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-page"></div>
+        <div className="clearfix"></div>
+
+        <div className="wrapper-page">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/main/resources/src/js/saiku/index.js
+++ src/main/resources/src/js/saiku/index.js
@@ -1,33 +1,13 @@
-'use strict';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Grid from './components/bootstrap/Grid';
-import Row from './components/bootstrap/Row';
-import Col from './components/bootstrap/Col';
-import Button from './components/bootstrap/Button';
-import Input from './components/bootstrap/Input';
-import FormGroup from './components/bootstrap/FormGroup';
+import { Router, Route } from 'react-router';
+import { createHistory } from 'history';
+import Login from './components/saiku/Login';
 
-window.onload = () => {
-  ReactDOM.render(
-    <Grid>
-      <Row>
-        <Col xs={12} md={8}>
-          <p>Hello!!</p>
-          <Button>Default</Button>
-          <Button bsStyle="primary" block>Primary</Button>
-          <Button href="#" bsStyle="primary" bsSize="large" disabled>Primary link</Button>
-          <Button bsStyle="primary" bsSize="large" active>Large button</Button>
-          <form>
-            <FormGroup>
-              <label htmlFor="exampleInputEmail1">Email:</label>
-              <Input type="email" id="exampleInputEmail1" placeholder="Email" />
-            </FormGroup>
-          </form>
-        </Col>
-      </Row>
-    </Grid>,
-    document.getElementsByClassName('saiku-app')[0]
-  );
-};
+var routes = (
+  <Router history={createHistory()}>
+    <Route path="/" component={Login} />
+  </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));
