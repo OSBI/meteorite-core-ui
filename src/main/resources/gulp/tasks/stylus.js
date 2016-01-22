a1a66179f3a7535947bdf08078a06c6af2aaa2b3
breno.polanski <breno.polanski@gmail.com>
2016-01-22 11:29:35 -0300

    Add postcss in task stylus

--- src/main/resources/gulp/tasks/stylus.js
+++ src/main/resources/gulp/tasks/stylus.js
@@ -1,15 +1,24 @@
 'use strict';
 
 // Necessary plugins
-var gulp        = require('gulp');
-var plumber     = require('gulp-plumber');
-var stylus      = require('gulp-stylus');
-var paths       = require('../paths');
+var gulp         = require('gulp');
+var plumber      = require('gulp-plumber');
+var stylus       = require('gulp-stylus');
+var poststylus   = require('poststylus');
+var autoprefixer = require('autoprefixer');
+var paths        = require('../paths');
 
 // Call Stylus
 module.exports = gulp.task('stylus', function() {
+  var processors = [
+    autoprefixer
+  ];
   return gulp.src(paths.source.files.styl)
     .pipe(plumber())
-    .pipe(stylus())
+    .pipe(stylus({
+      use: [
+        poststylus(processors)
+      ]  
+    }))
     .pipe(gulp.dest(paths.build.app));
 });
